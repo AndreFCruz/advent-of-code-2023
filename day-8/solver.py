fb5263e89915eac4ca2a2c48855d7fc16517d814
AndreFCruz <andrecruz97@gmail.com>
2023-12-08 16:50:20 +0100

    added day 8 problem sheet

---
+++ day-8/solver.py
@@ -0,0 +1,50 @@
+import re
+import sys
+import math
+import operator
+from dataclasses import dataclass
+from functools import reduce
+
+
+def parse_input(input_lines: list[str]):
+    pass
+
+
+def solve_part_one(problem_data) -> int:
+    """Solve part one.
+    """
+    pass
+
+
+def solve_part_two(problem_data) -> int:
+    """Solve part two.
+    """
+    pass
+
+
+def main():
+
+    # # Read input
+    # # > Load from stdin
+    # input_lines = sys.stdin.readlines()
+
+    # > Or load from file
+    from pathlib import Path
+    input_path = Path(__file__).parent / "example1.txt"
+    input_lines = [
+        line.strip() for line in input_path.read_text().split("\n")
+    ]
+
+    # Parse input
+    problem_data = parse_input(input_lines)
+
+    # Solve problem
+    output = solve_part_one(problem_data)
+    # output = solve_part_two(problem_data)
+
+    # Write to stdout
+    print(output, file=sys.stdout)
+
+
+if __name__ == "__main__":
+    main()
