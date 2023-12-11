0b26ea1931df15e1636c9aa21ff5932df0cb561a
AndreFCruz <andrecruz97@gmail.com>
2023-12-11 22:54:19 +0100

    day 9 part 1

--- day-9/solver.py
+++ day-9/solver.py
@@ -1,14 +1,51 @@
+import re
 import sys
 
 
 def parse_input(input_lines: list[str]):
-    pass
+    return [parse_numbers(line) for line in input_lines if line]
+
+
+def parse_numbers(line: str) -> list[int]:
+    numbers_regex = re.compile(r"[-]?\d+")
+    return [int(num) for num in numbers_regex.findall(line)]
 
 
 def solve_part_one(problem_data) -> int:
     """Solve part one.
     """
-    pass
+    return sum(
+        extrapolate_value_from_history(history)
+        for history in problem_data
+    )
+
+
+def extrapolate_value_from_history(history: list[int]) -> int:
+
+    solution_lines = [history]
+
+    # Construct pyramid lines
+    while True:
+        next_line = differences_between_adjacent_numbers(solution_lines[-1])
+        solution_lines.append(next_line)
+
+        # Stop if the next line is all zeros
+        if set(next_line) == {0}: break
+
+    # Reconstruct all extrapolated values and return the final extrapolated value
+    for idx in range(len(solution_lines) - 2, -1, -1):
+        extrapolated_val = solution_lines[idx][-1] + solution_lines[idx + 1][-1]
+        solution_lines[idx].append(extrapolated_val)
+    
+    print(history[:5], solution_lines[0][-1])
+    return solution_lines[0][-1]
+
+
+def differences_between_adjacent_numbers(numbers: list[int]) -> list[int]:
+    return [
+        numbers[i + 1] - numbers[i]
+        for i in range(len(numbers) - 1)
+    ]
 
 
 def solve_part_two(problem_data) -> int:
@@ -25,7 +62,7 @@ def main():
 
     # > Or load from file
     from pathlib import Path
-    input_path = Path(__file__).parent / "example1.txt"
+    input_path = Path(__file__).parent / "input.txt"
     input_lines = [
         line.strip() for line in input_path.read_text().split("\n")
     ]
