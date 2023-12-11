4a7ec8b7bacb535e3479d7e5427793c1f885f91a
AndreFCruz <andrecruz97@gmail.com>
2023-12-11 22:15:41 +0100

    day 8 part 1

--- day-8/solver.py
+++ day-8/solver.py
@@ -6,14 +6,37 @@ from dataclasses import dataclass
 from functools import reduce
 
 
-def parse_input(input_lines: list[str]):
-    pass
+def parse_input(input_lines: list[str]) -> tuple[str, dict[str, tuple[str, str]]]:
+    left_right_instructions = input_lines[0].strip()
+
+    graph = dict()
+    node_regex_line = re.compile(r"(?P<src>\w+) = [(](?P<left>\w+), (?P<right>\w+)[)]")
+    for line in input_lines[1:]:
+        if (match := node_regex_line.match(line)) is not None:
+            src = match.group("src")
+            left = match.group("left")
+            right = match.group("right")
+            graph[src] = (left, right)
+
+    return left_right_instructions, graph
 
 
 def solve_part_one(problem_data) -> int:
     """Solve part one.
     """
-    pass
+    instructions, graph = problem_data
+
+    num_steps = 0
+    curr_node = "AAA"
+    dst_node = "ZZZ"
+    curr_instr_idx = 0
+    while curr_node != dst_node:
+        instr = instructions[curr_instr_idx]
+        curr_node = graph[curr_node][0 if instr == "L" else 1]
+        num_steps += 1
+        curr_instr_idx = (curr_instr_idx + 1) % len(instructions)
+    
+    return num_steps
 
 
 def solve_part_two(problem_data) -> int:
@@ -30,7 +53,7 @@ def main():
 
     # > Or load from file
     from pathlib import Path
-    input_path = Path(__file__).parent / "example1.txt"
+    input_path = Path(__file__).parent / "input.txt"
     input_lines = [
         line.strip() for line in input_path.read_text().split("\n")
     ]
